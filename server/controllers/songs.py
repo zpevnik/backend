a3bfc134fe9e61330c5b54f3b8aaa91db5bfe1f7
Jakub Kulík <kulikjak@gmail.com>
2017-06-23 17:34:26 +0200

    current_user fix

--- server/controllers/songs.py
+++ server/controllers/songs.py
@@ -3,6 +3,7 @@ from flask import jsonify
 from flask import request
 from flask import Blueprint
 from flask_cors import cross_origin
+from flask_login import current_user
 from flask_login import login_required
 
 from server.app import app
@@ -96,7 +97,6 @@ def song_single(song_id):
                                  'data': song_id})
 
         return jsonify(), 204
-
 
 
 #@api.route('/songs/<song_id>', methods=['GET', 'PUT', 'DELETE'])
