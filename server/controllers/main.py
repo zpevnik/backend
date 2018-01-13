7bbff25c4ae2568a63234e7e01dce77f904d2a6d
Jakub Kulík <kulikjak@gmail.com>
2018-01-13 17:15:42 +0100

    Exports are now not send as attachments

--- server/controllers/main.py
+++ server/controllers/main.py
@@ -126,9 +126,9 @@ def download(filename):
     directory = os.path.join(os.getcwd(), app.config['SONGBOOK_DONE_FOLDER'])
     return send_from_directory(
         directory=directory,
-        filename=filename,
-        as_attachment=True,
-        attachment_filename="Zpevnik.pdf")
+        filename=filename)
+#        as_attachment=True,
+#        attachment_filename="Zpevnik.pdf")
 
 
 @app.route("/cleanup")  # NOT TESTED! TODO
